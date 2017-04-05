f263fe0f82966c94a658712203935fba58e69718
Markus Teich <markus.teich@stusta.mhn.de>
2017-04-05 02:11:58 +0200

    remove config widget

--- pidgin-gotr.c
+++ pidgin-gotr.c
@@ -221,34 +221,6 @@ onGotrRecvUser(void       *roomClosure,
 }
 
 
-GtkWidget *
-buildConfWidget(PurplePlugin *plugin)
-{
-	GtkWidget *vbox = gtk_vbox_new(FALSE, 5);
-	GtkWidget *notebook = gtk_notebook_new();
-	GtkWidget *configbox = gtk_vbox_new(FALSE, 5);
-	GtkWidget *fingerprintbox = gtk_vbox_new(FALSE, 5);
-
-//	gtk_container_set_border_width(GTK_CONTAINER(vbox), 2);
-	gtk_container_set_border_width(GTK_CONTAINER(configbox), 5);
-	gtk_container_set_border_width(GTK_CONTAINER(fingerprintbox), 5);
-
-	//TODO
-
-	gtk_notebook_append_page(GTK_NOTEBOOK(notebook),
-	                         configbox,
-	                         gtk_label_new("Config"));
-	gtk_notebook_append_page(GTK_NOTEBOOK(notebook),
-	                         fingerprintbox,
-	                         gtk_label_new("Known fingerprints"));
-
-	gtk_box_pack_start(GTK_BOX(vbox), notebook, TRUE, TRUE, 0);
-
-	gtk_widget_show_all(vbox);
-	return vbox;
-}
-
-
 gboolean
 checkKeygenDone(gpointer data)
 {
@@ -1171,9 +1143,6 @@ pluginUnload(PurplePlugin *plugin)
 }
 
 
-static PidginPluginUiInfo uiConfig = { buildConfWidget };
-
-
 static PurplePluginInfo info = {
 	PURPLE_PLUGIN_MAGIC,
 	PURPLE_MAJOR_VERSION,
@@ -1197,7 +1166,7 @@ static PurplePluginInfo info = {
 	pluginUnload,
 	NULL,
 
-	&uiConfig,
+	NULL, // conf widget generation function
 	NULL,
 	NULL,
 	NULL,
